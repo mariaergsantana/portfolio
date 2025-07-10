70b284b704c003cd04231fef082509bb3b1d9cba
Maria Gomes <mariaergsantana@outlook.com>
2025-07-10 00:29:33 -0300

    Assests
    1- Changed everything that had the name "assests" to "assets"

--- assests/javascript/main.js
+++
@@ -1,8 +0,0 @@
-const links = document.querySelectorAll('.nav_item');
-
-links.forEach(link => {
-  link.addEventListener('click', () => {
-    links.forEach(l => l.classList.remove('active'));
-    link.classList.add('active');
-  });
-});
